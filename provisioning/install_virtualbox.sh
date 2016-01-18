7151db66a8e8755ef004b8684e8d511221babf7b
c0state <stephen.liu@alumni.stanford.edu>
2016-01-18 17:07:02 -0500

    Remove virtualbox/vagrant from circle.yml as it's not supported (at least with xen), install python requirements

--- provisioning/install_virtualbox.sh
+++ provisioning/install_virtualbox.sh
@@ -1,13 +1,10 @@
 set -ex
 
 if ! grep --quiet "deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free" /etc/apt/sources.list.d/virtualbox.list; then
-  apt-get update
-  apt-get install linux-kernel-headers
-
   sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list"
   wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | apt-key add -
   apt-get update
-  apt-get install virtualbox-5.0
+  apt-get install virtualbox-5.0 -y
 fi
 
 echo "Successfully installed virtualbox"
