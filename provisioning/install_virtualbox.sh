616b36dbb6cfbeace9cc20c9a2a35df59f50d1d5
c0state <stephen.liu@alumni.stanford.edu>
2016-01-18 15:57:43 -0500

    Fix virtualbox install script to fail properly if an error occurs

--- provisioning/install_virtualbox.sh
+++ provisioning/install_virtualbox.sh
@@ -3,7 +3,8 @@ set -ex
 if ! grep --quiet "deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free" /etc/apt/sources.list.d/virtualbox.list; then
   sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list"
   wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
-  apt-get update && sudo apt-get install virtualbox-5.0
+  apt-get update
+  sudo apt-get install virtualbox-5.0
 fi
 
 echo "Successfully installed virtualbox"
