1187de4050aa400e4a58f393b5c45d695172c94a
c0state <stephen.liu@alumni.stanford.edu>
2016-01-18 16:06:06 -0500

    Install kernel headers prior to virtualbox

--- provisioning/install_virtualbox.sh
+++ provisioning/install_virtualbox.sh
@@ -1,6 +1,8 @@
 set -ex
 
 if ! grep --quiet "deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free" /etc/apt/sources.list.d/virtualbox.list; then
+  apt-get install linux-kernel-headers
+
   sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list"
   wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
   apt-get update
