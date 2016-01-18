7d0dea5988adf6e31bee397af2c807861bb14f67
c0state <stephen.liu@alumni.stanford.edu>
2016-01-18 16:18:47 -0500

    Update apt before installing

--- provisioning/install_virtualbox.sh
+++ provisioning/install_virtualbox.sh
@@ -1,12 +1,13 @@
 set -ex
 
 if ! grep --quiet "deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free" /etc/apt/sources.list.d/virtualbox.list; then
+  apt-get update
   apt-get install linux-kernel-headers
 
   sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list"
-  wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
+  wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | apt-key add -
   apt-get update
-  sudo apt-get install virtualbox-5.0
+  apt-get install virtualbox-5.0
 fi
 
 echo "Successfully installed virtualbox"
