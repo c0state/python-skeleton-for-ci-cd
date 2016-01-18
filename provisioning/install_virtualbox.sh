b687f36bc55ed4a5f32458a18a3ccc9ccfe49b4b
c0state <stephen.liu@alumni.stanford.edu>
2016-01-18 15:38:20 -0500

    Fix virtualbox install conditional clause

--- provisioning/install_virtualbox.sh
+++ provisioning/install_virtualbox.sh
@@ -1,7 +1,9 @@
 set -ex
 
-if ! grep --quiet "deb http://dl.google.com/linux/chrome/deb/ stable main" /etc/apt/sources.list.d/google-chrome.list; then
+if ! grep --quiet "deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free" /etc/apt/sources.list.d/virtualbox.list; then
   sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list"
   wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
   apt-get update && sudo apt-get install virtualbox-5.0
 fi
+
+echo "Successfully installed virtualbox"
